edb0e191abf9e5d5a45dc7b5417a55439ac0fb43
konomiyu <86641374+konomiyuzu@users.noreply.github.com>
2022-03-16 03:29:58 +0700

    Update 1.1.0

--- FreeNewtAltars/Export/README.md
+++ FreeNewtAltars/Export/README.md
@@ -6,6 +6,11 @@ Only the host needs the mod.
 
 ## Changelog
 
+**1.1.0**
+
+* Added an option to only activate the mod before/after a certain stage
+* Redid configuration file
+
 **1.0.0**
 
 * Inital Release.
--- FreeNewtAltars/FreeNewtAltars.cs
+++ FreeNewtAltars/FreeNewtAltars.cs
@@ -12,12 +12,23 @@ namespace FreeNewtAltars
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "konomiyu";
         public const string PluginName = "FreeNewtAltars";
-        public const string PluginVersion = "1.0.0";
-        private static ConfigEntry<int> NewtAltarPrice;
+        public const string PluginVersion = "1.1.0";
+        private static ConfigEntry<int> NewtAltarPrice, MinimumStageCount;
+        private static ConfigEntry<bool> StageMode, After;
         public void Awake()
         {
-            NewtAltarPrice = Config.Bind("", "NewtAltarPrice", 0, new ConfigDescription("Determines the price of newt altars"));
-            
+
+            //cleanup old
+            Config.Bind("", "NewtAltarPrice", 0, new ConfigDescription("Determines the price of newt altars"));
+            Config.Remove(new ConfigDefinition("", "NewtAltarPrice"));
+
+            NewtAltarPrice = Config.Bind("Price", "NewtAltarPrice", 0, new ConfigDescription("Determines the price of newt altars"));
+
+            StageMode = Config.Bind("Stage", "UseStage", false, new ConfigDescription("Determines if the mod is active before/after a set stage"));
+            MinimumStageCount = Config.Bind("Stage", "Stage", 5);
+            After = Config.Bind("Stage", "Before/After", true, new ConfigDescription("false = before\ntrue = after"));
+
+
             On.RoR2.SceneDirector.Start += SceneDirector_Start;
         }
 
@@ -29,11 +40,25 @@ namespace FreeNewtAltars
 
         private static void AdjustNewtPrice()
         {
+            int StageCount = Run.instance.stageClearCount + 1;
+            //if stage mode is on
+            if (StageMode.Value && (
+
+                // if current stage is <= stagecount and after is true
+                //this is for consistency with before, as in before the value is toggled at MinimumStageCount + 1
+                //without the "=" it would be toggled at MinimumStageCount
+                (StageCount <= MinimumStageCount.Value && After.Value) ||
+
+                // if current stage is > stagecount and after is false
+                (StageCount > MinimumStageCount.Value && After.Value == false)
+                )) return;
+
             var purchaseInteractions = InstanceTracker.GetInstancesList<PurchaseInteraction>();
             foreach (PurchaseInteraction purchaseInteraction in purchaseInteractions)
             {
                 if (purchaseInteraction.name.StartsWith("NewtStatue"))
                 {
+                    
                     purchaseInteraction.cost = NewtAltarPrice.Value;
                     if (NewtAltarPrice.Value == 0) purchaseInteraction.costType = CostTypeIndex.None;
                 }
