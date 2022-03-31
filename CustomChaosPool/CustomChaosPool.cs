98f0d752b4ebc45c09bb61838d394a14a556059f
potato-at-coding <86641374+potato-at-coding@users.noreply.github.com>
2022-03-31 22:54:32 +0700

    Update 1.1.0

--- CustomChaosPool/CustomChaosPool.cs
+++ CustomChaosPool/CustomChaosPool.cs
@@ -4,6 +4,11 @@ using RoR2;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
+using System;
+using System.Reflection;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
 namespace CustomChaosPool
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
@@ -12,9 +17,9 @@ namespace CustomChaosPool
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "konomiyu";
         public const string PluginName = "CustomChaosPool";
-        public const string PluginVersion = "1.0.3";
+        public const string PluginVersion = "1.1.0";
         public static ConfigEntry<string> Equipments, AddEquipments, SetEquipmentsString;
-        public static ConfigEntry<bool> Experimental, RemoveIncompatibleEquipments, SetEquipments, LogDebug, GiveEquipmentInfo, CapInsensitive, SpaceInsensitive;
+        public static ConfigEntry<bool> Experimental, RemoveIncompatibleEquipments, SetEquipments, LogDebug, GiveEquipmentInfo, CapInsensitive, SpaceInsensitive, SpecialInsensitive, TricornPatch;
         private static List<EquipmentIndex> RemoveList, AddList, ExactList;
         private static Dictionary<string, EquipmentIndex> Dict;
 
@@ -26,15 +31,19 @@ namespace CustomChaosPool
             ConfigInit();
             
 
-            //hook
+            //hooks
             On.RoR2.EquipmentCatalog.Init += EquipmentCatalog_Init;
-
+            IL.RoR2.EquipmentSlot.FireBossHunter += EquipmentSlot_FireBossHunter;
         }
+
+
+
         private void ConfigInit()
         {
             //config
             CapInsensitive = Config.Bind("Selection", "Capitalization Insensitivity", true, "Enables Capitalization insensitivity\nTry disabling this if a modded equipment has the same internal name but not capitalization");
             SpaceInsensitive = Config.Bind("Selection", "Space Insensitivity", true, "Enables Space insensitivity\nTry disabling this if a modded equipment has similiar names with different spaces");
+            SpecialInsensitive = Config.Bind("Selection","Special Character Insensitivity", true, "Enables Special character insensitivity\nthis includes all foreign language characters\nTry disabling this if a modded equipment consists of foreign language characters");
 
             Equipments = Config.Bind("Equipment", "Remove Equipment", "Volcanic Egg", "Determines what equipment are removed from the bottled chaos pool\nrefer to the mod's webpage for a guide");
 
@@ -43,6 +52,7 @@ namespace CustomChaosPool
             SetEquipments = Config.Bind("Experimental", "Use Exact Pool", false, "Allows you to set an exact pool");
             SetEquipmentsString = Config.Bind("Experimental", "Pool", "0,1,5,6,8,9,10,23,24,25,26,29,32,33,37,41,44,45,48,50", "(the default value is the default pool)\n(please note that if you have mods that adds equipment these indexes would be incorrect)\n(requires Experimental)");
             RemoveIncompatibleEquipments = Config.Bind("Experimental", "Auto Cleanup non default equipment", false, "Enables automatic clearing for equipment that are not in the default pool\nfor example: Tricorn, Unused Equipment, Lunar Equipment\n(requires Experimental)");
+            TricornPatch = Config.Bind("Experimental", "Trophy Hunter's Tricorn patch", false, "Enables a patch to stop your equipment from getting cosumed if trophy hunter's tricorn is in the bottled chaos pool\nmight break mods that alter tricorn\n(requires Experimental)");
 
             LogDebug = Config.Bind("Miscellaneous", "Log Debug", true, "Logs extra info for debugging");
             GiveEquipmentInfo = Config.Bind("Miscellaneous", "Give Equipment Info", false, "Logs the indexes, internal names and names of all equipments into the log\n(meant for use with modded equipment)");
@@ -51,11 +61,12 @@ namespace CustomChaosPool
         private static List<EquipmentIndex> ConfigParser(ConfigEntry<string> conf)
         {
             //Some mods might have the same internal name with different capitilization so im adding an option for lowercase and space insensitivity
+            //(and now special insensitivity)
             string equipments = conf.Value;
 
-            if(CapInsensitive.Value) equipments = equipments.ToLower();
+            if (CapInsensitive.Value) equipments = equipments.ToLower();
             if (SpaceInsensitive.Value) equipments = Regex.Replace(equipments, @"\s*", "");
-
+            if (SpecialInsensitive.Value) equipments = Regex.Replace(equipments, @"(?![\s\w]).", "");
 
             string[] arr = equipments.Split(',');
             List<EquipmentIndex> output = new List<EquipmentIndex>();
@@ -174,10 +185,17 @@ namespace CustomChaosPool
                     ExternalName = Regex.Replace(ExternalName, @"\s*", "");
                 }
 
+                //Remove Special option
+                if (SpecialInsensitive.Value)
+                {
+                    InternalName = Regex.Replace(InternalName, @"(?![\s\w]).", "");
+                    ExternalName = Regex.Replace(ExternalName, @"(?![\s\w]).", "");
+                }
+
                 //warn about duplicates
                 if (Dict.ContainsKey(InternalName) || Dict.ContainsKey(ExternalName)) Log.LogWarning($"Duplicate Internal/External name for Index {c}");
 
-                // check if != "" because a certain equipment has no internal name
+                // check if != "" because a certain equipment has no name
                 // and ignore duplicates as a last line of defense just in case
                 // but duplicates shouldn't make it here (hopefully)
                 Dict.Add($"{c}", (EquipmentIndex)c);
@@ -188,12 +206,47 @@ namespace CustomChaosPool
                 c++;
             }
 
-            if(GiveEquipmentInfo.Value)Log.LogInfo(Info);
+            if(GiveEquipmentInfo.Value)Log.LogMessage(Info);
             return Dict;
 
         }
 
+        private void EquipmentSlot_FireBossHunter(ILContext il)
+        {
+            //patch for tricorn
+            if (!TricornPatch.Value || !Experimental.Value) return;
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(
+                x => x.MatchDup(),
+                x => x.MatchBrtrue(out _),
+                x => x.MatchPop(),
+                x => x.MatchLdnull(),
+                x => x.MatchBr(out _),
+                x => x.MatchCall<CharacterBody>("get_inventory"),
+                x => x.MatchCall<UnityEngine.Object>("op_Implicit"),
+                x => x.MatchBrfalse(out _)
+                );
+            c.Index += 8;
+            c.RemoveRange(17);
+            c.Emit(OpCodes.Ldarg_0);
 
+            //Adds characterbody to the stack
+            c.Emit(OpCodes.Call, typeof(EquipmentSlot).GetMethod("get_characterBody"));
+            c.EmitDelegate<Action<CharacterBody>>((cb) =>
+            {
+
+                if (cb.inventory.currentEquipmentIndex != EquipmentCatalog.FindEquipmentIndex("BossHunter")) return;
+
+                //original code
+                CharacterMasterNotificationQueue.PushEquipmentTransformNotification(cb.master,
+                cb.inventory.currentEquipmentIndex, DLC1Content.Equipment.BossHunterConsumed.equipmentIndex,
+                CharacterMasterNotificationQueue.TransformationType.Default);
+                cb.inventory.SetEquipmentIndex(DLC1Content.Equipment.BossHunterConsumed.equipmentIndex);
+            });
+
+            Log.LogWarning("Tricorn Patch Applied");
+        }
     }
+
 
 }
--- CustomChaosPool/Export/README.md
+++ CustomChaosPool/Export/README.md
@@ -8,7 +8,7 @@
 Allows for full customization of the Bottled Chaos equipment pool via config file  
 should be compatible with mods as long as you can get their name/internal name/equipment index
 
->Only the host needs the mod? (untested)
+``Only the host needs the mod? (untested)``
 
 ## Config guide
 ### Input
@@ -26,6 +26,8 @@ The Config also has options for case and space insensitivity
 > **for example** : "vOLc aNic eGG " or "eccentricvase" can both be valid inputs	
 ### Config Options
 
+### Selection
+
 #### Capitalization Insensitivity
 
 > Enables Capitilization insensitivity  
@@ -38,15 +40,26 @@ The Config also has options for case and space insensitivity
 > **for example** : "Eccentric Vase" and "Ecc entri c Vase " will both work with this enabled  
 > Some modded equipment might have the same name with different Spacing so try disabling this if that's the case
 
+#### Special Character Insensitivity
+
+> Enables Special Character insensitivity  
+> **for example** : "Trophy Hunter's Tricorn" and "Trophy Hunters Tricorn" will both work with this enabled  
+> Some modded equipment might have the foreign character names which will get discarded by this option
+
+### Equipment
+
 #### Remove Equipment
 
 > Removes any selected equipment from the pool  
 > **for example** : "Jade Elephant, Sawmerang"  
 > this will remove jade elephant and sawmerang from the equipment pool
 
+### Experimental
+
 #### Enable Experimental settings
 > Enables experimental settings which might break things  
-> Such as allowing you to add elite aspects or trophy hunter to the equipment pool
+> Such as allowing you to add elite aspects or trophy hunter to the equipment pool  
+> Required for all other options in experimental
 
 #### Add Equipment
 > Adds any selected equipment to the pool  
@@ -64,6 +77,13 @@ The Config also has options for case and space insensitivity
 > **for example** : "Radar Scanner, Trophy Hunter's Tricorn"  
 > if Auto cleanup is enabled trophy hunter would be automatically removed  
 > this is meant to prevent mistakes
+
+#### Trophy Hunter's Tricorn patch
+> Enables a patch for trophy hunter's tricorn  
+> This patch fixes an issue where if trophy hunter's tricorn is added to the pool and is rolled it will replace your equipment with the consumed version of trophy hunter's tricorn regardless of your current equipment  
+> Might break other mods that modifies the trophy hunter's tricorn's code
+
+### Miscellaneous
 
 #### Log Debug
 > Logs extra info for debugging purposes
@@ -88,18 +108,28 @@ you can find those by enabling "Give Equipment Info" then looking at the log fil
 I hope this is easy enough to understand to a non mod developer ^v^
 
 ## Known Issues
-* If Trophy hunter is added to the pool and is rolled, it will replace your equipment with the consumed version of trophy hunter regardless of your equipment 
+* ``~~If Trophy hunter is added to the pool and is rolled, it will replace your equipment with the consumed version of trophy hunter regardless of your equipment~~``
 
-I might fix this if i ever feel motivated to (persumably by having a run ruined by it)
+there is an experimental patch for which may break mods that modifiy the tricorn
+
+* ``If 2 modded equipment somehow have the exact same name, capitalization, spacing and special characters the mod will ignore whichever one loads slower``
+
+though if this were to happen you can still select them via equipment index as that cannot be the same
 
 ## Planned features
 * ~~Maybe a way to view the equipment name/internal name/index of modded equipments~~ 🗸 implemented 
-* Special characters (such as ') insensitivity
+* ~~Special characters (such as ') insensitivity~~ 🗸 implemented 
 
 
 ## Contact
 You can find me in the modding discord **konomiyu#3611**, feel free to message me regarding any issues or suggestions for the mod
 ## Changelog
+
+**1.1.0**
+
+* Added Special Character insensitivity (with an option to disable it)
+* Added an experimental patch for trophy hunter's tricorn
+* Minor internal changes
 
 **1.0.3**
 
